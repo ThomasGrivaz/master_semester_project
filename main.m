59306a4280c8fc0f901ed3bb2f7944153eee21e7
Thomas Grivaz <thomas.grivaz@epfl.ch>
2016-03-03 13:06:08 +0100

    .

--- main.m
+++ main.m
@@ -9,12 +9,13 @@ close all;
 
 load data/train.mat;
 
-% we keep only the 10 first samples to test
-X = train.images(1:1000,:);
-y = train.labels(1:1000,:);
+% we keep only the 100 first samples to test
+X = train.images(1:100,:);
+y = train.labels(1:100,:);
 
 % normalise the features
-%X = gradient_descent_preprocessing(X);
+%X = gradient_descent_preprocessing(X); 
+% features are already normalised, sparse matrix leads to NaN
 
 %% Gradient descent
 objFct = objective_fct_builder('MSE', X, y);
